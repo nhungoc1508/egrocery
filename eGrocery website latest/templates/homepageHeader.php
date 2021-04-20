48473071d737fd5c44c513d0cb3c67123ed3a18e
LiamRM <lrm444@nyu.edu>
2021-04-20 17:30:43 +0400

    ":art: Reformatted and organized code"

--- eGrocery website latest/templates/homepageHeader.php
+++ eGrocery website latest/templates/homepageHeader.php
@@ -3,22 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Latest compiled and minified CSS -->
+    <!-- Best practice: only CSS files are included with "link rel="stylesheet", not Javascript. CSS files are also added to the 
+      head of the HTML file so styles are loaded before the user accesses the website -->
+    <!-- Bootstrap CSS, then optional theme, then Google Fonts, then custom header.css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- jQuery Library -->
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-    <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <script defer src="https://kit.fontawesome.com/6e02521c5e.js" crossorigin="anonymous"></script>
-    <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:wght@700&family=Potta+One&display=swap" rel="stylesheet">
-
-    <script src="" charset="utf-8"></script>
     <link rel="stylesheet" href="header.css">
 
     <title>HomepageHeader</title>
@@ -76,5 +67,12 @@
       </nav>
     </header>
 
+    <!-- Best practice: Javascript links are typically added at the end of the HTML body 
+    to give HTML time to load before JS loads (speeds up response time and prevents some errors) -->
+    <!-- jQuery first, then minified JS, then fontawesome, then iconify -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script defer src="https://kit.fontawesome.com/6e02521c5e.js" crossorigin="anonymous"></script>
+    <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
   </body>
 </html>
