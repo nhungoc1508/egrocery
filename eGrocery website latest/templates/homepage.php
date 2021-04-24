ed6bee8f484f6f3c2c8d7775c85c8261c5b57a8e
Ngoc Hoang <ngochoang@Ngoc-Hoangs-Mac.local>
2021-04-24 15:04:08 +0400

    Minor changes

--- eGrocery website latest/templates/homepage.php
+++ eGrocery website latest/templates/homepage.php
@@ -1,14 +1,22 @@
 <?php
+include("new_navbar.php");
 include("new_header.php");
-// include("homepageHeader.php");
 include("database.php");
 
-// setcookie('name','',Time()-3600);
+setcookie('name','',Time()-3600);
 
-//  $search_product = $_GET['search_product'];
+ $search_product = $_GET['search_product'];
 
-//  $select_query = "SELECT * FROM `products` WHERE `product_name` LIKE '%$search_product%'";
-//  $select_query_result = mysqli_query($con,$select_query) or die(mysqli_error($con));
+ $select_query = "SELECT
+ *
+FROM
+ products
+ORDER BY
+ product_price
+ 
+DESC
+LIMIT 12";
+ $select_query_result = mysqli_query($con,$select_query) or die(mysqli_error($con));
 
 //  include('homepageContent.php');
  include('new_footer.php');
