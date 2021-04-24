8e1e4044bacc78acbc16d26bdf3fb26be5656801
Uttam <ukm202@nyu.edu>
2021-04-24 17:17:49 +0400

    links to category and search created

--- eGrocery website latest/templates/database.php
+++ eGrocery website latest/templates/database.php
@@ -4,6 +4,6 @@
 
 
 
-$con = mysqli_connect("localhost","root","","eGrocery") or die(mysqli_error($con));
+$con = mysqli_connect("localhost","root","root","eGrocery") or die(mysqli_error($con));
 
 ?>
--- eGrocery website latest/templates/homepage.php
+++ eGrocery website latest/templates/homepage.php
@@ -4,25 +4,42 @@ include("new_navbar.php");
 include("new_header.php");
 include("database.php");
 
-setcookie('name','',Time()-3600);
+// setcookie('name','',Time()-3600);
 
- $search_product = $_GET['search_product'];
+//
+if ($_GET['search_product']){
 
- $select_query = "SELECT
- *
-FROM
- products
-ORDER BY
- product_price
- 
-DESC
-LIMIT 12";
+  $search_product = $_GET['search_product'];
+
+  $select_query = "SELECT * FROM `products` WHERE `product_name` LIKE '%$search_product%'";
+
+}
+else if ($_GET['category']){
+  $search_product = $_GET['category'];
+  print $search_product;
+  // include("'$search_product'.category_header.php");
+
+  $select_query = "SELECT * FROM `products` WHERE `category` LIKE '%$search_product%'";
+
+}
+else{
+
+   $select_query = "SELECT
+   *
+  FROM
+   products
+  ORDER BY
+   product_price
+
+  DESC
+  LIMIT 12";
+}
+
  $select_query_result = mysqli_query($con,$select_query) or die(mysqli_error($con));
 
-//  include('homepageContent.php');
-// include('homepageHeaderNEW.php');
-//include('catalog.php');
+
 include("catalog.php");
+
 include("new_footer.php");
 
 
