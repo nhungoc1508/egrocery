430e7fadc0f367a9840d49c55f9ed040d77924ba
Ngoc Hoang <ngochoang@Ngoc-Hoangs-Mac.local>
2021-04-24 11:29:19 +0400

    Added new header and footer

---
+++ eGrocery website latest/templates/new_footer.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <!-- This line of code allows for a user to automatically download Bootstrap CSS files onto their browser via a CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+    <!-- Customized CSS for all pages -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="new_header_footer.css">
+    <title>Sketch</title>
+</head>
+
+<body>
+    <div class="container-fluid card">
+        <div class="">
+            <div class="row mb-4 ">
+                <div class="col-md-6 col-sm-11 col-xs-11">
+                    <div class="footer-text pull-left">
+                        <div class="col-12">
+                            <div class="d-flex">
+                                <h1 class="display-5">eGrocery</h1>
+                            </div>
+                            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi non pariatur numquam animi nam at impedit odit nisi.</p>
+                            <div class="divider mb-4"> </div>
+                            <h5 class="display-5">Contact</h5>
+                            <ul class="no-bullets">
+                                <div class="social mt-2 mb-3"><i class="fas fa-map-marker-alt"></i> PO Box 129188, Saadiyat Island, Abu Dhabi, United Arab Emirates</div>
+                                <div class="social mt-2 mb-3"><i class="fas fa-phone-alt"></i> +971 (0)47 382 9374</div>
+                                <div class="social mt-2 mb-3"><i class="fas fa-envelope"></i> contact@egrocery.com</div>
+                                <div class="social mt-2 mb-3"><i class="fas fa-lock"></i> Secured online payment <i class="fab fa-cc-mastercard fa-lg"></i><i class="fab fa-cc-visa"></i><i class="fab fa-cc-paypal"></i><i class="fab fa-cc-amex"></i></div>
+                                <div class="social mt-2 mb-3"><i class="fab fa-facebook-f fa-lg"></i><i class="fab fa-instagram fa-lg"></i></i><i class="fab fa-twitter fa-lg"></i><i class="fab fa-linkedin fa-lg"></i></div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-4">
+                    <h5 class="display-5">Categories</h5>
+                    <ul class="no-bullets">
+                        <li>Beverages</li>
+                        <li>Dairy</li>
+                        <li>Desserts</li>
+                        <li>Dry goods</li>
+                        <li>Fruits</li>
+                        <li>Vegetables</li>
+                        <li>Meat</li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-4">
+                    <h5 class="display-5">Information</h5>
+                    <ul class="no-bullets">
+                        <li>About us</li>
+                        <li>Contact</li>
+                        <li>Terms & conditions</li>
+                        <li>Return & exchange</li>
+                        <li>Shipping & delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="divider mb-4"> </div>
+            <div class="row d-flex justify-content-center" style="font-size:10px;">
+                <p><i class="fa fa-copyright"></i> 2021</p>
+            </div>
+        </div>
+    </div>
+    <!-- JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
