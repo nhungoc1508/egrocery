56200bb5c6c9e96d10a2bf78502b776e69fbf7a2
LiamRM <lrm444@nyu.edu>
2021-04-24 18:02:46 +0400

    :rocket: more register page fields

--- eGrocery website latest/templates/register.php
+++ eGrocery website latest/templates/register.php
@@ -33,8 +33,12 @@
                 <input type="text" class="form-control" id="lname" aria-describedby="lastNameHelp" placeholder="Enter last name" required>
             </div>
             <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter username" required>
+                <label for="phone">Phone Number</label>
+                <input type="tel" class="form-control" id="phone" aria-describedby="phoneHelp" placeholder="Enter phone number" required>
+            </div>
+            <div class="form-group">
+                <label for="address">Address</label>
+                <input type="text" class="form-control" id="address" aria-describedby="addressHelp" placeholder="Enter address" required>
             </div>
             <div class="form-group">
                 <label for="email">Email address</label>
@@ -42,25 +46,34 @@
                 <small id="emailHelp" class="form-text text-muted">**We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter username" required>
+            </div>
+            <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
             </div>
             <div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">I agree to the following terms and conditions</label>
-                <!-- <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Right?">Dismissible popover</a> -->
+                <label class="form-check-label" for="exampleCheck1">I agree to the following <a tabindex="0" class="" role="button" data-toggle="popover" data-trigger="focus" title="Terms & Conditions" data-content="I agree to give each member of this project a final grade of A.">terms and conditions.</a></label>
             </div>
             <button type="submit" class="btn btn-primary">Create Account</button>
         </form>
     </div>
-    <!-- <script>
+    <!-- JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script>
         $(function () {
             $('[data-toggle="popover"]').popover()
         })
         $('.popover-dismiss').popover({
             trigger: 'focus'
         })
-    </script> -->
+    </script>
 </body>
 
 </html>
