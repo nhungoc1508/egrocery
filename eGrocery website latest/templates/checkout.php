e3fb5abc082e4bff6bc819a43d7a228cc716bce8
LiamRM <lrm444@nyu.edu>
2021-04-28 23:37:15 +0400

    :rocket: checkout payment fields

--- eGrocery website latest/templates/checkout.php
+++ eGrocery website latest/templates/checkout.php
@@ -35,12 +35,8 @@ $total = 0;
 
             <div class="row title-row">
 
-                <div class="col-lg-2 mb-4">
-                    <!-- Empty Space -->
-                </div>
-
                 <!--Grid column-->
-                <div class="col-lg-8 mb-4">
+                <div class="col-lg-8 mb-4 mx-auto">
 
                     <h3>Delivery Address</h3>
 
@@ -141,23 +137,60 @@ $total = 0;
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 mb-4">
-                    <!-- Empty Space -->
-                </div>
                 <!-- Card -->
 
-                <div class="row product-row">
-                </div>
-                <div class="row price-row">
-                    <h3 class="subtotal-text col-md-12 float-right">Subtotal:</h3>
-                    <h2 class="subtotal-price col-md-12 float-right">AED</h2>
-                    <div class="container-fluid confirm-payment-ctn">
-                        <button type="button" class="btn btn-primary btn-confirm-payment float-right">Confirm Payment</button>
+            </div>
+
+
+            <!-- Billing Information Card -->
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <h3>Billing Information</h3>
+                    <div class="card card-billing">
+                        <!-- credit card info-->
+                        <div id="credit-card" class="tab-pane fade show active pt-3">
+                            <form role="form" onsubmit="event.preventDefault()">
+                                <div class="form-group"> <label for="username">
+                                        <h6>Card Owner</h6>
+                                    </label> <input type="text" name="username" placeholder="Card Owner Name" required class="form-control "> </div>
+                                <div class="form-group">
+                                    <label for="cardNumber">
+                                        <h6>Card number</h6>
+                                    </label>
+                                    <div class="input-group"> <input type="text" name="cardNumber" placeholder="Valid card number" class="form-control " required>
+                                        <div class="input-group-append"> <span class="input-group-text text-muted"> <i class="fab fa-cc-visa mx-1"></i> <i class="fab fa-cc-mastercard mx-1"></i> <i class="fab fa-cc-amex mx-1"></i> </span> </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <div class="form-group"> <label><span class="hidden-xs">
+                                                    <h6>Expiration Date</h6>
+                                                </span></label>
+                                            <div class="input-group"> <input type="number" placeholder="MM" name="" class="form-control" required> <input type="number" placeholder="YY" name="" class="form-control" required> </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group mb-4">
+                                            <label data-toggle="tooltip" title="Three digit CV code on the back of your card">
+                                                <h6>CVV <i class="fa fa-question-circle d-inline"></i></h6>
+                                            </label>
+                                            <input type="text" required class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="payment-row">
+                                    <h3 class="subtotal-text col-md-12 float-right">Subtotal:</h3>
+                                    <h2 class="subtotal-price col-md-12 float-right">AED</h2>
+                                </div>
+                                <button type="button" class="subscribe btn btn-primary btn-block shadow-sm"> Confirm Payment </button>
+                            </form>
+                        </div> <!-- End -->
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <?php
     include("new_footer.php");
 
